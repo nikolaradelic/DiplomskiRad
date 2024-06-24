14ebcca7087ab49c60fd5bd20d91dc5c5cee331f
Nikola Radelić <51515434+nikolaradelic@users.noreply.github.com>
2024-06-24 17:56:31 +0200

    Update and rename test.py to haversine_distance_calc.py

--- haversine_distance_calc.py
+++ haversine_distance_calc.py
@@ -11,18 +11,7 @@ if __name__ == '__main__':
     inputPath = sys.argv[1]
     outputPath = sys.argv[2]
 
-    spark = SparkSession.builder \
-    	.config("spark.sql.session.timeZone", "UTC") \
-        .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
-        .config("spark.rapids.sql.exec.CollectLimitExec", "true") \
-        .config("spark.executor.memory", "12g") \
-    	.config("spark.driver.memory", "4g") \
-    	.config("spark.default.parallelism", "100") \
-    	.config("park.memory.offHeap.enabled", "true") \
-    	.config("spark.memory.offHeap.size", "4g") \
-    	.config("spark.driver.maxResultSize", "2g") \
-    	.config("spark.sql.shuffle.partitions", "200") \
-    	.getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
     
     # Register the UDF
     spark.udf.registerJavaFunction("computeHaversine", "com.example.haversinedistance.HaversineDistance", None)
@@ -34,6 +23,7 @@ if __name__ == '__main__':
     # Compute the haversine distance using the UDF
     df = df.filter("lat1 is not NULL and lon1 is not NULL and lat2 is not NULL and lon2 is not NULL")
     df = df.selectExpr('lat1', 'lon1', 'lat2', 'lon2', 'computeHaversine(lon1, lat1, lon2, lat2) as distance')
+    df = df.sort(col("distance"))
     
     # Write the output data
     start_time = time.time()
